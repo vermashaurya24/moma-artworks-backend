0680c401d7552f0e578cd888a79caa3c154703cb
Shaurya Raj Verma <vermashaurya24@gmail.com>
2024-03-20 00:36:01 +0545

    Implemented security middlewares RateLimit, Helmet and performance middleware Compression.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
 const errorHandler = require("./middlewares/error-handler");
 require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
@@ -10,8 +13,18 @@ const artworksRouter = require("./routes/artworks");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
 app.use(express.json());
 app.use(cors());
+
+app.use(helmet());
+
+app.use(compression());
 
 app.use("/swagger-ui", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
